0108614ba5576d9dcb42667fcc8e7232d1b89686
NuclearReactor314 <145520137+NuclearReactor314@users.noreply.github.com>
2023-12-08 12:08:24 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // 示例数据
+    const data = [
+        { nickname: 'Comparethee', QQnumber: '3260135199', fan: '二代团', reject: '无', goodimpression: '二代4，7', other: '无'},
+        // 添加更多数据...
+    ];
+
+    // 获取DOM元素
+    const searchInput = document.getElementById('searchInput');
+    const infoTableBody = document.querySelector('#infoTable tbody');
+
+    // 初始化表格
+    renderTable(data);
+
+    // 搜索框事件监听
+    searchInput.addEventListener('input', function () {
+        const searchTerm = searchInput.value.toLowerCase();
+        const filteredData = data.filter(item =>
+            item.name.toLowerCase().includes(searchTerm) ||
+            item.age.toString().includes(searchTerm) ||
+            item.city.toLowerCase().includes(searchTerm)
+        );
+        renderTable(filteredData);
+    });
+
+    // 渲染表格
+    function renderTable(data) {
+        // 清空表格
+        infoTableBody.innerHTML = '';
+
+        // 遍历数据并添加到表格
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `<td>${item.name}</td><td>${item.age}</td><td>${item.city}</td>`;
+            infoTableBody.appendChild(row);
+        });
+    }
+});
